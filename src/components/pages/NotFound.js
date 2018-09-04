3e7c830045789ca94c5e42ab571f9ef09cca5fd6
unknown <chauhan.kunal.996@gmail.com>
2018-09-04 18:33:52 +0530

    initial commit

---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () =>{
+  return (
+    <div>
+      <h1 className="display-4"><span className="text-danger">404: </span>Page not found</h1>
+      <p className="lead">That page does not exist</p>
+    </div>
+  )
+}
+
+export default NotFound;
